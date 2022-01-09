58e6051bc424fcf4df507d1b1fddbc6c7e28167c
Salmanabdullah <salmandcc@gmail.com>
2022-01-09 01:31:56 +0100

    ......

--- Color Flipper/colorFlipper.js
+++ Color Flipper/colorFlipper.js
@@ -1,1 +1,8 @@
-const color =["green","red","blue","yellow"];
+const colors =["green","red","blue","yellow"];
+const button = document.getElementById("button");
+const color = document.querySelector(".color");
+
+button.addEventListener('click',function(){
+    const randomNumber = 2;
+    document.body.style.backgroundColor =colors[randomNumber]
+})
