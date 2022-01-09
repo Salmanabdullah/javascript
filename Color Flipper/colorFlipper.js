7d4e288ef87998c37425b2e38e1408f557ae9da6
Salmanabdullah <salmandcc@gmail.com>
2022-01-09 22:08:25 +0100

    ..

--- Color Flipper/colorFlipper.js
+++ Color Flipper/colorFlipper.js
@@ -5,10 +5,12 @@ const color = document.querySelector(".color");
 btn.addEventListener('click',function(){
     // select color randomly from colors
     const randomNumber = getRandomNumber();
+    //console.log(randomNumber);
     document.body.style.backgroundColor =colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber(){
+    //return Math.random();
     return Math.floor(Math.random() * colors.length);
 }
