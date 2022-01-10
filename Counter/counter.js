4c57e5b320472db870c8b6e6d4f5c7882082cb15
Salmanabdullah <salmandcc@gmail.com>
2022-01-10 10:09:32 +0100

    Update counter.js

--- Counter/counter.js
+++ Counter/counter.js
@@ -9,21 +9,28 @@ const btns = document.querySelectorAll('.btn');
 btns.forEach(function(btn){   
  btn.addEventListener('click', function(e){
      //accessing event object
-    const select = e.target.classList;
+    const styles = e.target.classList;
 
     //select specific classes
-    if (select.contains('decrease')) {
+    if (styles.contains('decrease')) {
         count--;
-    } else if(select.contains('increase')){
+    } else if(styles.contains('increase')){
         count++;
     } else {
         count = 0;
-    };
+    }
+    if (count > 0){
+        value.style.color = 'green'
+    }
+    else if (count < 0){
+        value.style.color = 'red'
+    }
+    else{
+        value.style.color = 'black'
+    }
     value.textContent = count;
 
  });
 
 });  
-
-
 
