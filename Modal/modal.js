ee3a2df948abfb623c88217a63c25ac0bce036c3
Salmanabdullah <salmandcc@gmail.com>
2022-01-13 14:35:25 +0100

    Create modal.js

---
+++ Modal/modal.js
@@ -0,0 +1,13 @@
+// define variables for modal button, modal and closing button
+const btn = document.querySelector('.btn')
+const mdl = document.querySelector('.modal')
+const closeBtn = document.querySelector('.close-btn')
+
+//click on modal button will add open-modal class
+btn.addEventListener('click', function(){
+    mdl.classList.add('open-modal')
+})
+// click on close-btn will remove the class 
+closeBtn.addEventListener('click', function(){
+    mdl.classList.remove('open-modal')
+})
