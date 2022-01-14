6f51b49f9431c81bce8da3467249754272fd6706
Salmanabdullah <salmandcc@gmail.com>
2022-01-14 21:10:44 +0100

    Update menu.js

--- Our Menu/menu.js
+++ Our Menu/menu.js
@@ -1,3 +1,5 @@
+//a menu array
+// learn how to get this data using ajax
 const menu = [
     {
       id: 1,
@@ -9,35 +11,63 @@ const menu = [
     },
     {
       id: 2,
-      title: "beef burger",
+      title: "chicken burger",
       category: "breakfast",
-      price: 15.99,
+      price: 10.99,
       img: "./images/item-1.jpg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
       id: 3,
-      title: "beef burger",
+      title: "lamb burger",
       category: "breakfast",
-      price: 15.99,
+      price: 18.99,
       img: "./images/item-1.jpg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
       id: 4,
-      title: "beef burger",
+      title: "fish burger",
       category: "breakfast",
-      price: 15.99,
+      price: 12.99,
       img: "./images/item-1.jpg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
       id: 5,
-      title: "beef burger",
+      title: "vegie burger",
       category: "breakfast",
-      price: 15.99,
+      price: 10.99,
       img: "./images/item-1.jpg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
   ];
 
+// when my page loads i need to call a callback function
+// so i have selected the root class for all the menu items
+
+const parentItem = document.querySelector('.section-center');
+
+//target the whole page using window and then listen to DOMContentLoaded
+// then call a callback function
+window.addEventListener('DOMContentLoaded', function(){
+  // console.log("hey there");
+  // map method to iterate through each item of the array 
+  // also i can change items using map
+  let displayMenu = menu.map(function(item) {
+    // return `<h1>${item.title}</h1>`; // returns title of every items
+    return `<article class="menu-item">
+    <img src=${item.img} alt=${item.title} class="photo">
+    <div class="item-info">
+        <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">&euro;${item.price}</h4>
+        </header>
+        <p class="item-description">${item.desc}</p>
+    </div>
+</article>`;
+  });
+  displayMenu = displayMenu.join('');
+  // console.log(displayMenu);
+  parentItem.innerHTML = displayMenu;
+})
