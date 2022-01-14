383cc327b5066a800d192d53177c3cd1d712f19d
Salmanabdullah <salmandcc@gmail.com>
2022-01-14 01:44:49 +0100

    Create menu.js

---
+++ Our Menu/menu.js
@@ -0,0 +1,43 @@
+const menu = [
+    {
+      id: 1,
+      title: "beef burger",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/menu-item.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "beef burger",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/menu-item.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 3,
+      title: "beef burger",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/menu-item.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 4,
+      title: "beef burger",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/menu-item.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 5,
+      title: "beef burger",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/menu-item.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+  ];
+
