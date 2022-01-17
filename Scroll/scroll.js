340cbd7052b820d492aab9399956d15008dcd762
Salmanabdullah <salmandcc@gmail.com>
2022-01-17 21:05:53 +0100

    Update scroll.js

---
+++ Scroll/scroll.js
@@ -0,0 +1,12 @@
+//setting up the date
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+//toggle functionalities
+const toggleBtn = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+toggleBtn.addEventListener('click', function(){
+    linksContainer.classList.toggle('show-links')
+})
