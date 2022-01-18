7a1703f676df153dbb8bfc650b5d29c8624dc13e
Salmanabdullah <salmandcc@gmail.com>
2022-01-18 20:20:55 +0100

    Update scroll.js

--- Scroll/scroll.js
+++ Scroll/scroll.js
@@ -6,10 +6,15 @@ date.innerHTML = new Date().getFullYear();
 const toggleBtn = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
+/*clicking on toggle button will toggle a new class show-links on the parent DOM
+which is defined on css file*/
 
 toggleBtn.addEventListener('click', function(){
     linksContainer.classList.toggle('show-links')
 })
+/*scrolling down after a certain height will fixed the navbar by adding
+a new class changed-nav and another new class to act as top button to go
+back to the top.  */
 const navBar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
 
@@ -24,11 +29,25 @@ window.addEventListener('scroll', function(){
         topLink.classList.remove('show-arrow-sign')
     }
 })
+
+/*smooth scrolling */
 const scrolling = document.querySelectorAll('.scroll-link')
 scrolling.forEach(function(link){
     link.addEventListener('click',function(e){
         e.preventDefault();
-        const linksTarget = e.currentTarget.getAttribute('href');
-        console.log(linksTarget);
+        const linksTarget = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(linksTarget)
+        const navHeight = navBar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navBar.classList.contains('changed-nav');
+        let offSetTop = element.offsetTop - navHeight;
+        if(!fixedNav){
+            offSetTop = offSetTop - navHeight;
+        }
+        if(navHeight > 150){
+            offSetTop = offSetTop + containerHeight;
+        }
+        window.scrollTo({left:0, top:offSetTop,});
+        linksContainer.classList.remove('show-links')
     })
 })
