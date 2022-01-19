2a133f569ba5acc1173604a7725ade103ea9ecec
Salmanabdullah <salmandcc@gmail.com>
2022-01-19 16:28:16 +0100

    Update countdown.js

--- Countdown Timer/countdown.js
+++ Countdown Timer/countdown.js
@@ -1,3 +1,12 @@
+/**name of months and weekdays => make arrays
+ * target the containers in html
+ * separate year, month, day, hour, min and sec from Date()
+ * get the time duration when offer will expire
+ * calculate that duration and extract miliseconds into  day, hour, min and sec format
+ * use an array of day, hour, min and sec to call the values and add in the innerHTML
+ * add a zero if it goes below 10
+ * use setInterval method to refresh the time automatically
+*/
 const months = [
     'January',
     'February',
@@ -21,12 +30,13 @@ const months = [
     'Friday',
     'Saturday',
   ];
-
+// giveway date and time setup
 const giveway = document.querySelector('.giveway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let givewayDate = new Date(2022, 9, 20, 18, 30, 0);
+let givewayDate = new Date(2022, 1, 11, 18, 30, 0);
+// console.log(givewayDate);
 
 const year = givewayDate.getFullYear();
 const hours = givewayDate.getHours();
@@ -39,5 +49,50 @@ month = months[month]
 let day = givewayDate.getDay();
 day = weekdays[day];
 
-console.log(date);
+// console.log(date);
 giveway.textContent = `giveway ends on ${day}, ${date} ${month} ${year} ${hours}:${mins}`;
+
+/*futute time calculation. getTime() returns in miliseconds.*/ 
+const futureTime = givewayDate.getTime();
+console.log(futureTime);
+
+/**Function to convert miliseconds into days, hours, mins and secs */
+function remainingTime(){
+  const timeRightNow = new Date().getTime();
+  const totalTime = futureTime - timeRightNow;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  
+  //calculate all values
+  let days = totalTime / oneDay;
+  days = Math.floor(days)
+  
+  let hours = Math.floor((totalTime % oneDay) / oneHour);
+  // hour = Math.floor(hour)
+  
+  let mins = (totalTime % oneHour) / oneMinute;
+  mins = Math.floor(mins)
+  
+  let secs = (totalTime % oneMinute) / 1000;
+  secs = Math.floor(secs)
+  
+  //value array
+  const values = [days,hours,mins,secs];
+  
+  function format(item){
+    if(item < 10){
+      return `0${item}`;
+    }
+    return item;
+  }
+
+  items.forEach(function(item,index){
+    item.innerHTML = format(values[index]);
+  })
+}
+//countdown
+let countdown = setInterval(remainingTime,1000)
+
+
+remainingTime();
