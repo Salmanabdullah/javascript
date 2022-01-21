a89f05b979e27ae38c410c2c88a9cf10a5bbbb60
Salmanabdullah <salmandcc@gmail.com>
2022-01-21 02:08:37 +0100

    Update grocery.js

--- Grocery Bud/grocery.js
+++ Grocery Bud/grocery.js
@@ -10,7 +10,8 @@ const clearBtn = document.querySelector('.clear-btn');
 let editFlag = false;
 
 //event listeners
-form.addEventListener('submit',addItem)
+form.addEventListener('submit',addItem);
+clearBtn.addEventListener('click',clearItems);
 
 //functions
 function addItem(e){
@@ -31,7 +32,11 @@ function addItem(e){
                     <button class="delete-btn" type="button">
                         <i class="fas fa-trash"></i>
                     </button>
-                </div>`
+                </div>`;
+      const deleteBtn = element.querySelector('.delete-btn'); 
+      const editBtn = element.querySelector('.edit-btn');
+      deleteBtn.addEventListener('click', deleteItem);
+      editBtn.addEventListener('click', editItem);
        //append child
         list.appendChild(element);
         // display alert
@@ -58,5 +63,47 @@ function displayAlert (text, action){
     setTimeout(function(){
         alert.textContent = '';
         alert.classList.remove(`alert-${action}`);
-    },1000)
+    },2000)
+}
+//set back to default
+function setBackToDefault(){
+    grocery.value = '';
+    editID = '';
+    editFlag = false;
+    submitBtn.textContent = 'submit';
+}
+
+//add to local storage
+function addToLocalStorage(id,value){
+    // console.log('dsfdsf');
+}
+
+//clear items function
+function clearItems(){
+    const items = document.querySelectorAll('.grocery-item');
+
+    if(items.length > 0){
+        items.forEach(function(item){
+            list.removeChild(item);
+        });
+    }
+    container.classList.remove('show-container');
+    displayAlert('Items has been removed','success');
+    setBackToDefault();
+}
+
+//delete Item 
+function deleteItem(e){
+   const element = e.currentTarget.parentElement.parentElement;
+   list.removeChild(element);
+   if(list.children.length === 0){
+       container.classList.remove('show-container');
+   }
+   displayAlert('Item has been removed','success');
+    setBackToDefault();
+}
+//edit item
+function editItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+
 }
