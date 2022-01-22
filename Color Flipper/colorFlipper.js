e91092a7b058dc91498e79c8037db4182e46ac33
Salmanabdullah <salmandcc@gmail.com>
2022-01-22 17:35:22 +0100

    Update colorFlipper.js

--- Color Flipper/colorFlipper.js
+++ Color Flipper/colorFlipper.js
@@ -1,16 +1,20 @@
-const colors =["green","red","blue","yellow"];
+//setting variables
+const colors =["green",'#37C268',"red",'#C1FCBC',"blue","yellow"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener('click',function(){
+/******Event listeners ***********/
+btn.addEventListener('click',colorChanger);
+
+/********Functions *********/
+function colorChanger(){
     // select color randomly from colors
     const randomNumber = getRandomNumber();
     //console.log(randomNumber);
     document.body.style.backgroundColor =colors[randomNumber];
     color.textContent = colors[randomNumber];
-});
+}
 
 function getRandomNumber(){
-    //return Math.random();
     return Math.floor(Math.random() * colors.length);
 }
