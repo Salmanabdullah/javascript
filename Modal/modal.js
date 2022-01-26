a9405ac9fd423534cf6c37199f316bb0463f129a
Salmanabdullah <salmandcc@gmail.com>
2022-01-26 17:45:39 +0100

    Update modal.js

--- Modal/modal.js
+++ Modal/modal.js
@@ -6,8 +6,8 @@ const closeBtn = document.querySelector('.close-btn')
 //click on modal button will add open-modal class
 btn.addEventListener('click', function(){
     mdl.classList.add('open-modal')
-})
+});
 // click on close-btn will remove the class 
 closeBtn.addEventListener('click', function(){
     mdl.classList.remove('open-modal')
-})
+});
