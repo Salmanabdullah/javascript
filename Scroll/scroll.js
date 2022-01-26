38ac564c8000e9a23c4b8aed87f27400967faee7
Salmanabdullah <salmandcc@gmail.com>
2022-01-26 17:45:32 +0100

    Update scroll.js

--- Scroll/scroll.js
+++ Scroll/scroll.js
@@ -13,7 +13,7 @@ toggleBtn.addEventListener('click', function(){
     linksContainer.classList.toggle('show-links')
 })
 /*scrolling down after a certain height will fixed the navbar by adding
-a new class changed-nav and another new class to act as top button to go
+a new class 'changed-nav' and another new class to act as go-top button to go
 back to the top.  */
 const navBar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
@@ -31,7 +31,7 @@ window.addEventListener('scroll', function(){
 })
 
 /*smooth scrolling */
-const scrolling = document.querySelectorAll('.scroll-link')
+const scrolling = document.querySelectorAll('.scroll-link');
 scrolling.forEach(function(link){
     link.addEventListener('click',function(e){
         e.preventDefault();
