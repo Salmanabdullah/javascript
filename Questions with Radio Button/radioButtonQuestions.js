84a760a72f341bb440be29b0fbb4486384ffd1c2
Salmanabdullah <salmandcc@gmail.com>
2022-01-28 01:53:47 +0100

    Update radioButtonQuestions.js

--- Questions with Radio Button/radioButtonQuestions.js
+++ Questions with Radio Button/radioButtonQuestions.js
@@ -1,8 +1,11 @@
 const correctAnswers = ['B','B','A','B','A'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.score');
 
+/**Event Listeners */
 form.addEventListener('submit',collectAnswers);
 
+/**Functions */
 function collectAnswers(e){
     e.preventDefault();
     const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,]
@@ -12,7 +15,8 @@ function collectAnswers(e){
         if(answer ===correctAnswers[index]){
             score += 20;
         }
-        
+        result.querySelector('span').textContent = score + '%';
+        result.classList.remove('d-none');
+        scrollTo(0,0)
     });
-    console.log(score);
 }
