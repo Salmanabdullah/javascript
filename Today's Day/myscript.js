9de089748792af1dda73d8620a2042bc614d91aa
Salmanabdullah <salmandcc@gmail.com>
2022-01-30 19:18:58 +0100

    Update myscript.js

--- Today's Day/myscript.js
+++ Today's Day/myscript.js
@@ -1,9 +1,12 @@
-var date = new Date();
-var weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const date = new Date();
+const weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-var todaysDay = weekday[date.getDay()];
 
-function day(){
-    document.getElementById('weekday').innerHTML = todaysDay;
-}
-day();
+document.getElementById('weekday')
+    .innerHTML = weekday[date.getDay()];
+    /**getDay() returns a number. 0 means sunday, 1 means monday and so on */
+    
+    
+/***using dateFns. starts from (1-7). 1 means monday, 7 is sunday */    
+// console.log(weekday[7 - dateFns.format(date,'E')]);
+    
