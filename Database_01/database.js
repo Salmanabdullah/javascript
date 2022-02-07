f9b10e44ae5ce11189ca10176166599d8535d588
Salmanabdullah <salmandcc@gmail.com>
2022-02-07 18:11:35 +0100

    Update database.js

--- Database_01/database.js
+++ Database_01/database.js
@@ -5,17 +5,18 @@ const form = document.querySelector('form')
 //calling from firebase database, get documents
 db.collection('recipes').get().then(snapshot =>{
     snapshot.docs.forEach(element => {
-        addRecipe(element.data())
+        addRecipe(element.data(), element.id)
     });
 }).catch(err => console.log(err))
 
-/*******functions */
-function addRecipe(item){
+/*******functions */ 
+function addRecipe(item, id){
     let time = item.created_at.toDate();
     let html = `
-    <li>
+    <li data-id = ${id}>
     <div>${item.title}</div>
     <div>${time}</div>
+    <button class = "btn btn-danger btn-sm my-2">delete</button>
     </li>
     `;
     list.innerHTML += html
@@ -33,4 +34,13 @@ form.addEventListener('submit', e => {
     db.collection('recipes').add(recipe).then(()=> {
         console.log('recipe added');
     }).catch(err => console.log(err))
+})
+
+//deleting data
+list.addEventListener('click', e =>{
+    if(e.target.tagName === 'BUTTON'){
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id)
+            .delete().then(()=> console.log('recipe deleted'))
+    }
 })
