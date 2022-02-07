a7ea6b40d45baa4c28c477a36e4ab4e82584e1c4
Salmanabdullah <salmandcc@gmail.com>
2022-02-07 23:26:07 +0100

    Update chat.js

---
+++ chat_project/scripts/chat.js
@@ -0,0 +1,39 @@
+class Chatroom{
+    constructor(room,username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');     //from firebase
+    }
+    //chat functionalities
+    async addChat(message){
+        const now = new Date();                 //used in Timestamp
+        const chat = {                          //format a chat object
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        }
+        //adding to the database
+        const addToDatabase = await this.chats.add(chat);
+        return addToDatabase;
+    }
+    getchats(callback){
+        this.chats.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                const changeDoc = change.doc;
+                if(change.type === 'added'){
+                    //update ui
+                    callback(change.doc.data())
+                }
+            })
+        })
+    }
+}
+
+const chatroom = new Chatroom('gaming','salman');
+
+// chatroom.addChat('knock on the door')
+//     .then(() => console.log('chat added'))
+//     .catch(err => console.log(err))
+
+chatroom.getchats(data => console.log(data))
