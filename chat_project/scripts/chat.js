5e540135a62ef2ae50fbe4a0d882d31824c0b7d1
Salmanabdullah <salmandcc@gmail.com>
2022-02-08 20:23:56 +0100

    Update chat.js

--- chat_project/scripts/chat.js
+++ chat_project/scripts/chat.js
@@ -3,8 +3,9 @@ class Chatroom{
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');     //from firebase
+        this.getData;
     }
-    //chat functionalities
+    //adding messages (a Promise)
     async addChat(message){
         const now = new Date();                 //used in Timestamp
         const chat = {                          //format a chat object
@@ -18,25 +19,36 @@ class Chatroom{
         return addToDatabase;
     }
     getchats(callback){
-        this.chats
-        .where('room','==',this.room)                   //to set a condition
-        .orderBy('created_at') 
-        .onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(change => {
-                const changeDoc = change.doc;
-                if(change.type === 'added'){
-                    //update ui
-                    callback(change.doc.data())
-                }
+        this.getData = this.chats
+            .where('room','==',this.room)                   //to set a condition
+            .orderBy('created_at') 
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    const changeDoc = change.doc;
+                    if(change.type === 'added'){
+                        //update ui
+                        callback(change.doc.data())
+                    }
+                })
             })
-        })
+    }
+    //updating Username
+    updateName(username){
+        this.username = username;
+        localStorage.setItem('username', username)
+    }
+    updateRoom(room){
+        this.room = room;
+        if(this.getData){
+            this.getData()
+        }
     }
 }
 
-const chatroom = new Chatroom('gaming','salman');
+// const chatroom = new Chatroom('gaming','salman');
 
 // chatroom.addChat('are you there!')
 //     .then(() => console.log('chat added'))
 //     .catch(err => console.log(err))
 
-chatroom.getchats(data => console.log(data))
+// chatroom.getchats(data => console.log(data))
