c859c78e703cc8aca5d24463c77fea743cd7ca40
Salmanabdullah <salmandcc@gmail.com>
2022-02-08 20:24:00 +0100

    Update ui.js

---
+++ chat_project/scripts/ui.js
@@ -0,0 +1,23 @@
+class ChatUI{
+    constructor(list){
+        this.list = list
+    }
+    render(data){
+        const submitTime = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),
+            {addSuffix:true}
+        )
+        const html = `
+        <li class="list-group-item">
+            <span class="username">${data.username}:</span>
+            <span class="message">${data.message}</span>
+            <div class="time">${submitTime}</div>
+        </li>
+        `;
+
+        this.list.innerHTML += html;
+    }
+    clear (){
+        this.list.innerHTML = "";
+    }
+}
